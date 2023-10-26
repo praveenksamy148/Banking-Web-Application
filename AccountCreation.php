698cdb9362c0a1989fee9e9aa21ae0dcf52c1a31
And1deng <147691970+And1deng@users.noreply.github.com>
2023-10-25 22:50:22 -0700

    Add files via upload
    Added partial PHP database connection (NOT CURRENTLY WORKING)

--- AccountCreation.php
+++ AccountCreation.php
@@ -21,7 +21,7 @@
         <h3> Please follow the steps below to create a new account:  </h3>
       </div>
       <div class="Form">
-        <form action="" method="post">
+        <form action="AccountCreation.php" method="post">
             <label for="acc_name">Account Name:</label>
             <input type="text" id="acc_name" name="acc_name" required>
             <br><br>
@@ -37,10 +37,95 @@
             <label for="deposit">Initial Deposit:</label>
             <input type="number" id="deposit" name="deposit" required>
             <br><br>
-            
+
             <input type = "submit" value = "Submit" name = "submit" >
         </form>
       </div>
+          <?php
+          if(isset($_POST["submit"]))
+          {
+              $accName = $_POST["acc_name"];
+              $accID = $_POST["accID"];
+              $balID1 = $_POST["balID"];
+              $balType1 = $_POST["acc_type"];
+              $balID1_Balance = $_POST["deposit"];
+
+
+              $conn = mysqli_connect("localhost", "root", "", "users");
+              if (!$conn)
+              {
+                die("Connection failed: " . mysqli_connect_error());
+              }
+
+              $errors = array();
+              if(empty($accName) OR empty($balType1) OR empty($balID1_Balance)
+              {
+                  array_push($errors, "Required to fill out all fields!");
+              }
+
+              require_once "database.php";
+              $dupAccName = "SELECT * FROM acc_info WHERE accName = '$accName'";
+              $copied = mysqli_query($connection, $dupAccName);
+              if(!$copied || mysqli_num_rows($copied) > 0)
+              {
+                  array_push($errors, "Account name already exists");
+              }
+              if(count($errors) > 0)
+              {
+                  foreach($errors as $error)
+                  {
+                      echo "<div class= 'alert alert-danger'>$error</div>";
+                  }
+              }
+              else
+              {
+                $check = false;
+                while($check == false)
+                {
+                  $possibleID = rand(11111111111, 99999999999);
+                  $accIDCheck = "SELECT * FROM acc_info WHERE accID = '$possibleID'";
+                  $result = mysqli_query($connection, $accIDCheck);
+                  if(!$result)
+                  {
+                    die("Query failed: ". mysqli_error($connection));
+                  }
+                  else
+                  {
+                    $accID = $accIDCheck;
+                    $check = true;
+                  }
+                }
+
+                $check2 = false;
+                while($check2 == false)
+                {
+                  $possibleID = rand(11111111111, 99999999999);
+                  $balIDCheck = "SELECT * FROM acc_info WHERE balID1 = '$possibleID'";
+                  $result2 = mysqli_query($connection, $accIDCheck);
+                  if(!$result2)
+                  {
+                    die("Query failed: ". mysqli_error($connection));
+                  }
+                  else
+                  {
+                    $balID1 = $accIDCheck;
+                    $check2 = true;
+                  }
+                }
+                  $sql = "INSERT INTO acc_info (accName, accID, balID1, balType1, balID1_Balance) VALUES ('$accName', '$accID', '$balID1', '$balType1', '$balID1_Balance')";
+                  $results = mysqli_query($connection, $sql);
+                  if($results)
+                  {
+                      header("Location: Home.html");
+                  }
+                  else
+                  {
+                      echo "<div class= 'alert alert-danger'>Unable to Register</div>";
+                      echo mysqli_error($connection);
+                  }
+              }
+          }
+           ?>
 
     <script src = "script.js"></script>
   </body>
