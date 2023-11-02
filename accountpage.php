b28b8c11b1a6478d25f45ed9ae2c18c4c0087868
connor-corona-sjsu <connor.corona@sjsu.edu>
2023-11-02 11:11:26 -0700

    Update accountpage.php
    user dashboard updated to display account number, account type, and balances.

--- accountpage.php
+++ accountpage.php
@@ -17,26 +17,29 @@ if(!isset($_SESSION["user"])){
     }
     
     $numOfAccounts = 0; 
-    $sql = "SELECT * FROM acc_info";
+    $sql = "SELECT balance, account_id, account_type FROM acc_info";
     $result = mysqli_query($conn, $sql); 
-
-    // $accountInfoMatrix = array (
-    //     $accountIdArray => array();
-
-
-    // )
     
-    $accountIdArray = array();
+    $accountInfoMatrix = array (
+        "accountId" => array(),
+        "balance" => array(),
+        "accountType" => array()
+    );
 
     if ($result) {
         while ($row = mysqli_fetch_assoc($result)){
-            $accountIdArray[] = $row["account_id"];
+            $accountInfoMatrix["accountId"][] = $row["account_id"];
+            $accountInfoMatrix["balance"][] = $row["balance"];
+            if ($row["account_type"] == 1) {
+                $accountInfoMatrix["accountType"][] = "Checking";
+            } else {
+                $accountInfoMatrix["accountType"][] = "Savings";
+            }
+            $numOfAccounts++;
         }
     }
 
 ?>
-
-
 <html lang="en">
     <head>
         <title>Account Balances - Bank of MUSA</title>
@@ -44,12 +47,9 @@ if(!isset($_SESSION["user"])){
         <link rel="stylesheet" href="styles.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="BANK Checking and Savings Accounts viewpage">
-    </head>
-   
-   <!--Alt shadows for the accounts box-shadow: rgba(80, 23, 23, 0.4) 5px 5px, rgba(80, 23, 23, 0.3) 10px 10px, rgba(80, 23, 23, 0.2) 15px 15px, rgba(80, 23, 23, 0.1) 20px 20px, rgba(80, 23, 23, 0.05) 25px 25px;-->
-   
+    </head> 
     <header>
-        <a href="Home.html"><img id='logo' width='300' height='50' src="logo.png"></a>
+        <a href="MusaHome.html"><img id='logo' width='300' height='50' src="logo.png"></a>
         <div class="navbar"><a href='MusaHome.html'>Home</a></div>
         <div class="navbar"><a href='NewAccConfirm.php'>Checking & Savings</a></div>       
         <div class="navbar"><a href='deposits.html'>Make a Deposit</a></div>      
@@ -64,9 +64,9 @@ if(!isset($_SESSION["user"])){
             <div class="account-container-top">
                 <h1>Ignore this.</h1>
             </div>
-            <h1>Checking ...3151</h1>
+            <h1><?php echo $accountInfoMatrix["accountType"][0]?> ...<?php echo $accountInfoMatrix["accountId"][0] ?></h1>
             <h3>Available balance</h3>
-            <h2>$20,000.00</h2>
+            <h2>$<?php echo $accountInfoMatrix["balance"][0]?></h2>
             <div class="account-options-container">
                 <p class="account-options"><a href='accountdetails.html'>Account Details</a></p>
                 <p class="account-options"><a href='carddetails.html'>My Card</a></p>
@@ -75,35 +75,33 @@ if(!isset($_SESSION["user"])){
                 <p class="account-options"><a href='deposits.html'>Deposit</a></p>
             </div>
         </div>
-        <div class="account-container">
-            <div class="account-container-top">
-                <h1>Ignore this.</h1>
-            </div>
-            <h1>Savings ...3011</h1>
-            <h3>Available balance</h3>
-            <h2>$1,245,028.04</h2>
-            <div class="account-options-container">
-                <p class="account-options"><a href='accountdetails.html'>Account Details</a></p>
-                <p class="account-options"><a href='carddetails.html'>My Card</a></p>
-                <p class="account-options"><a href='bills.html'>Pay Bills</a></p>
-                <p class="account-options"><a href='transfers.html'>Transfer</a></p>
-                <p class="account-options"><a href='deposits.html'>Deposit</a></p>
-            </div>
-        </div>
-        <div class="account-container">
-            <div class="account-container-top">
-                <h1>Ignore this.</h1>
-            </div>
-            <h1>Savings ...2987</h1>
-            <h3>Available balance</h3>
-            <h2>$134.46</h2>
-            <div class="account-options-container">
-                <p class="account-options"><a href='accountdetails.html'>Account Details</a></p>
-                <p class="account-options"><a href='carddetails.html'>My Card</a></p>
-                <p class="account-options"><a href='bills.html'>Pay Bills</a></p>
-                <p class="account-options"><a href='transfers.html'>Transfer</a></p>
-                <p class="account-options"><a href='deposits.html'>Deposit</a></p>
-            </div>
-        </div>
+        <?php
+            // Class attributes constants 
+            $accountContainerClass = "account-container";
+            $accountContainerTopClass = "account-container-top";
+            $accountOptionsContainerClass = "account-options-container";
+            $accountOptionsClass = "account-options";
+
+            for ($i = 1; $i < $numOfAccounts; $i++) {
+                echo "<div class=\"$accountContainerClass\">
+                    <div class=\"$accountContainerTopClass\">
+                        <h1>Ignore this.</h1>
+                    </div>
+                    <h1>" . $accountInfoMatrix["accountType"][$i] . " ..." . $accountInfoMatrix["accountId"][$i] . "</h1>
+                    <h3>Available balance</h3>
+                    <h2>$" . $accountInfoMatrix["balance"][$i] . "</h2>
+                    <div class=\"$accountOptionsContainerClass\">
+                        <p class=\"$accountOptionsClass\"><a href='accountdetails.html'>Account Details</a></p>
+                        <p class=\"$accountOptionsClass\"><a href='carddetails.html'>My Card</a></p>
+                        <p class=\"$accountOptionsClass\"><a href='bills.html'>Pay Bills</a></p>
+                        <p class=\"$accountOptionsClass\"><a href='transfers.html'>Transfer</a></p>
+                        <p class=\"$accountOptionsClass\"><a href='deposits.html'>Deposit</a></p>
+                    </div>
+                </div>";
+
+
+            }
+
+        ?>
     </body>
 </html>
