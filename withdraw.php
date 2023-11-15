c3d9ab188edb48c180cd0d54049035db5b3dc93d
Praveen Kandasamy <pravee.samy@gmail.com>
2023-11-14 20:34:58 -0800

    documentation for each bank functionality stores in database

--- withdraw.php
+++ withdraw.php
@@ -1,27 +1,33 @@
 <?php 
-session_start(); 
-if(!isset($_SESSION["authenticate"])){
-    header("location: login.php");
-    exit(); 
-}else{
-    $timeout_duration = 900; 
-    $time_lastLogin = time() - $_SESSION['last_login_timestamp']; 
-    if($time_lastLogin > $timeout_duration){
-        header('Location: login.php'); 
+    session_start(); 
+    if(!isset($_SESSION["authenticate"])){
+        header("location: login.php");
         exit(); 
     }else{
-        $remaining_time = $timeout_duration - $time_lastLogin; 
-        $_SESSION['last_login_timestamp'] = time(); 
+        $timeout_duration = 900; 
+        $time_lastLogin = time() - $_SESSION['last_login_timestamp']; 
+        if($time_lastLogin > $timeout_duration){
+            session_unset();
+            session_destroy();
+            header('Location: login.php'); 
+            exit(); 
+        }else{
+            $remaining_time = $timeout_duration - $time_lastLogin; 
+            $_SESSION['last_login_timestamp'] = time(); 
+            
+        }
         
     }
-    
-}
-
-require_once "database.php"; // Include your database connection file at the beginning
 ?>
 
 <html lang="en">
 <head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Banking Account Creation</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="regStyling.css">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Playfair+Display:wght@700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="withdraw.css">
 </head>
 
@@ -42,7 +48,7 @@ require_once "database.php"; // Include your database connection file at the beg
             document.getElementById('time').textContent = countdown; 
 
         </script>
-        <h4>Live Session</h4>
+        <h4>Session Countdown: </h4>
         <h4 id = "time"> &nbsp Minutes:
             <script type="text/javascript">
             document.write(minutes)
@@ -121,7 +127,15 @@ require_once "database.php"; // Include your database connection file at the beg
                         $updateResult = $connection->query($updateSql);
                         if ($updateResult) 
                         {
-                            echo "SUCCESS: Your new balance in that account is: $newMoney"; 
+                            echo "<div class='alert alert-success'>Successfully Withdrew From Account!</div>";
+                            require_once "database.php"; 
+                            $userID = $_SESSION["userID"]; 
+                            $transaction = "Withdrew $". $amount ." from Account Number: " . $selectedAcc; 
+                            $transactions = "INSERT INTO user_transactions (accID, transaction) VALUES ('$userID', '$transaction')"; 
+                            $document = $connection->query($transactions); 
+                            if(!$document){
+                                die("Failed to upload documentation"); 
+                            }
                         } 
                         else 
                         {
@@ -146,13 +160,17 @@ require_once "database.php"; // Include your database connection file at the beg
         
         ?>
         </form>
-        <?php
-            $connection->close();
-        ?>
-        <div>
-            <button onclick = "window.location.href = 'accountpage.php'" class = "homeButton" >Back to Home Page</button>
-        </div>
     </div>
     </div>
+    <script>
+    var countdown = <?php echo json_encode($remaining_time); ?>; 
+    var timer = setInterval(function() {
+        countdown--;
+        var minutes = Math.floor(countdown / 60); 
+        var seconds = countdown % 60; 
+        document.getElementById('time').textContent = minutes + " Minutes : " + seconds + " Seconds"; 
+        if(countdown <= 0) clearInterval(timer);
+    }, 1000);
+</script>
 </body>
 </html>
